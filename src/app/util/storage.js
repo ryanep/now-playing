704222c7c7ce641e55a9ac9c20015f8802c6b67d
Taylor Winfield <taylorwinfield4@gmail.com>
2017-05-18 23:05:32 +0100

    add storage util

---
+++ src/app/util/storage.js
@@ -0,0 +1,15 @@
+import {
+  SPOTIFY_ACCESS_TOKEN,
+  SPOTIFY_REFRESH_TOKEN
+} from "../constants/storage-keys";
+
+export default store => {
+  store.subscribe(() => {
+    let { auth: { accessToken, refreshToken } } = store.getState();
+    if (accessToken && refreshToken) {
+      localStorage.setItem(SPOTIFY_ACCESS_TOKEN, accessToken);
+      localStorage.setItem(SPOTIFY_REFRESH_TOKEN, refreshToken);
+      console.log("written");
+    }
+  });
+};
