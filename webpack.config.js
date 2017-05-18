6b148f9999d78a1ae20bf95de32b976b4bcbdd69
Taylor Winfield <taylorwinfield4@gmail.com>
2017-05-18 23:06:45 +0100

    add token callback function to App

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,42 @@
-const webpack = require('webpack');
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const  fs = require('fs');
-let nodeModules = {}
-fs.readdirSync('node_modules').forEach(function (module) {
-    if (module !== '.bin') nodeModules[module] = 'commonjs ' + module
+const fs = require("fs");
+let nodeModules = {};
+fs.readdirSync("node_modules").forEach(function(module) {
+  if (module !== ".bin") nodeModules[module] = "commonjs " + module;
 });
 
 module.exports = [
-    {
-        name: "browser",
-        entry: path.join(__dirname, "src/client/index.js"),
-        output: {
-            path: path.join(__dirname, "dist/public/"),
-            filename: "build.js"
+  {
+    name: "browser",
+    entry: path.join(__dirname, "src/client/index.js"),
+    output: {
+      path: path.join(__dirname, "dist/public/"),
+      filename: "build.js"
+    },
+    module: {
+      loaders: [
+        {
+          test: /.js$/,
+          loader: "babel-loader",
+          include: path.resolve(__dirname, "src")
         },
-        module: {
-            loaders: [
-                {
-                    test: /.js$/,
-                    loader: 'babel-loader',
-                    include: path.resolve(__dirname, "src")
-                },
-                {
-                    test: /\.scss$/,
-                    include: path.resolve(__dirname, "src"),
-                    loader: ExtractTextPlugin.extract(['css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]', 'sass-loader'])
-                }
-            ]
-        },
-        plugins: [
-            new ExtractTextPlugin({ filename: 'style.css', allChunks: true }),
-            new webpack.optimize.UglifyJsPlugin(),
-            new webpack.optimize.OccurrenceOrderPlugin()
-        ]
-    }
+        {
+          test: /\.scss$/,
+          include: path.resolve(__dirname, "src"),
+          loader: ExtractTextPlugin.extract([
+            "css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]",
+            "sass-loader"
+          ])
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin({ filename: "style.css", allChunks: true }),
+      // new webpack.optimize.UglifyJsPlugin(),
+      new webpack.optimize.OccurrenceOrderPlugin()
+    ]
+  }
 ];
