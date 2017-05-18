e413936c68acff500ce9b96382d8eabd9bad12ba
Taylor Winfield <taylorwinfield4@gmail.com>
2017-05-18 23:06:08 +0100

    Add login and track components

---
+++ src/app/components/login/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => (
+  <a
+    href="https://accounts.spotify.com/authorize?client_id=4ea54ce3001542bdb54efbff4e75c91b&amp;response_type=code&amp;redirect_uri=http://localhost:3000/cb"
+  >
+    Login to Spotify
+  </a>
+);
