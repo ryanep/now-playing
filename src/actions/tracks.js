2508575d0879a792e238fa6e214ff2532b2bf68e
Ryan Elliott-Potter <ryan@ryanep.com>
2017-06-09 21:53:14 +0100

    Dispatch track success action only when track is new

--- src/actions/tracks.js
+++ src/actions/tracks.js
@@ -2,12 +2,17 @@ import * as actionTypes from "../constants/action-types";
 import SpotifyService from "../services/spotify";
 const spotifyService = new SpotifyService();
 
+let currentTrackID = 0;
+
 export function getCurrentTrack() {
   return dispatch => {
     dispatch(trackRequested());
     spotifyService.getCurrentTrack()
       .then(response => {
-        dispatch(getCurrentTrackSuccess(response));
+        if (currentTrackID !== response.data.item.id) {
+          dispatch(getCurrentTrackSuccess(response));
+          currentTrackID = response.data.item.id;
+        }
       })
       .catch(err => {
         throw Error("Error fetching current track");
