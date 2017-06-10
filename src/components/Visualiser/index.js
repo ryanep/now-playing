bdff3b3e3f994a777a933a10ec92d068ff1323cf
Ryan Elliott-Potter <ryan@ryanep.com>
2017-06-10 11:45:04 +0100

    Modify visualiser amount and styling

--- src/components/Visualiser/index.js
+++ src/components/Visualiser/index.js
@@ -4,10 +4,10 @@ import styles from './style.scss';
 export default class Visualiser extends Component {
 
 	render() {
-		const totalBars = 10;
+		const totalBars = 12;
 		return (
 			<div className={styles.visualiser}>
-				{[...Array(totalBars)].map((x, i) => <div className={styles.bar}></div>)}
+				{[...Array(totalBars)].map((x, i) => <div key={i} className={styles.bar}></div>)}
 			</div>
 		)
 	}
