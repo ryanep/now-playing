f3b985b5939adb3c97ea0c11635d3ba787c3af81
Ryan Elliott-Potter <ryan@ryanep.com>
2017-06-21 21:49:15 +0100

    Track added user now fetched when a new track is played

--- src/reducers/tracks.js
+++ src/reducers/tracks.js
@@ -10,22 +10,40 @@ export default function tracks(state = initialState, action) {
     case actionTypes.TRACK_CHANGED:
       const { track } = action;
       const { name, album, duration_ms } = action.track.item;
-      return { ...state, previousTracks: [...state.previousTracks, state.currentTrack], currentTrack: {
-        isPlaying: track.is_playing,
-        progress: track.progress_ms,
-        duration: duration_ms,
-        title: name,
-        artist: album.artists[0].name,
-        artwork: album.images
-      }};
+      return {
+        ...state,
+        previousTracks: [...state.previousTracks, state.currentTrack],
+        currentTrack: {
+          isPlaying: track.is_playing,
+          progress: track.progress_ms,
+          duration: duration_ms,
+          title: name,
+          artist: album.artists[0].name,
+          artwork: album.images
+        }
+      };
     case actionTypes.TRACK_UPDATED:
       const updatedTrack = state.currentTrack;
       updatedTrack.isPlaying = action.track.is_playing;
       updatedTrack.progress = action.track.progress_ms;
-      return { ...state, currentTrack: updatedTrack};
+      return { ...state, currentTrack: updatedTrack };
     case actionTypes.TRACK_REQUEST_FAILURE:
       // Do API error handling here
       return { ...state };
+    case actionTypes.TRACK_USER_SUCCESS:
+      const displayName = action.user.display_name || action.user.id;
+      const photo = action.user.images.length > 0 ? action.user.images[0].url : null;
+      
+      return {
+        ...state,
+        currentTrack: {
+          ...state.currentTrack,
+          added: {
+            name: displayName,
+            photo: photo
+          }
+        }
+      };
     default:
       return state;
   }
--- src/services/spotify.js
+++ src/services/spotify.js
@@ -1,11 +1,56 @@
 import { SPOTIFY_ACCESS_TOKEN } from "../constants/storage-keys";
 
 export default class SpotifyService {
-  apiURL = "https://api.spotify.com/v1/me/player/currently-playing";
+  apiURL = "https://api.spotify.com/v1";
 
   getCurrentTrack() {
     const accessToken = localStorage.getItem(SPOTIFY_ACCESS_TOKEN);
-    return fetch(`${this.apiURL}`, {
+    return fetch(`${this.apiURL}/me/player/currently-playing`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+      .then(response => response.json())
+      .catch(error => Promise.reject(error));
+  }
+
+  async getPlaylistTracks(userID, playlistID, offset = 0) {
+    const accessToken = localStorage.getItem(SPOTIFY_ACCESS_TOKEN);
+    const data = await fetch(`${this.apiURL}/users/${userID}/playlists/${playlistID}/tracks?offset=${offset}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+    return data.json();
+  }
+
+  async getAllPlaylistTracks(userID, playlistID) {
+    let tracks = [];
+    let totalTracks = 1;
+
+    while (tracks.length < totalTracks) {
+      let newTracks = await this.getPlaylistTracks(userID, playlistID, tracks.length);
+      tracks = [...tracks, ...newTracks.items];
+      totalTracks = newTracks.total;
+    }
+
+    return tracks;
+  }
+
+  async getUserFromPlaylist(userID, playlistID, trackID) {
+    const tracks = await this.getAllPlaylistTracks(userID, playlistID);
+    return tracks.find(trackData => {
+      return trackData.track.id === trackID;
+    });
+  }
+
+  getUser(userID) {
+    const accessToken = localStorage.getItem(SPOTIFY_ACCESS_TOKEN);
+    return fetch(`${this.apiURL}/users/${userID}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
