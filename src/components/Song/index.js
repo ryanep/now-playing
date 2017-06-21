83f1f2a4134a8b13a50d85c829eec6b881a340fa
Ryan Elliott-Potter <ryan@ryanep.com>
2017-06-21 19:15:59 +0100

    Move added by position and improve styling

--- src/components/Song/index.js
+++ src/components/Song/index.js
@@ -1,33 +1,31 @@
 import React from "react";
 import Progress from "../Progress";
 import Visualiser from "../Visualiser";
-import ProgressiveImage from 'react-progressive-image';
+import ProgressiveImage from "react-progressive-image";
 import styles from "./style.scss";
 
 export default ({ title, artist, artwork, added, isPlaying, progress, duration }) => (
   <div className={styles.song}>
     <div className={styles.album}>
       <ProgressiveImage src={artwork[0].url} placeholder={artwork[artwork.length - 1].url}>
-        {(src) => <img src={src} alt={`${artist} - ${title}`} className={styles.artwork} />}
+        {src => <img src={src} alt={`${artist} - ${title}`} className={styles.artwork} />}
       </ProgressiveImage>
       <div className={styles.track}>
-        <h1 className={styles.title}>{title}</h1>
-        <h2 className={styles.artist}>{artist}</h2>
+        <div className={styles.trackInfo}>
+          <h1 className={styles.title}>{title}</h1>
+          <h2 className={styles.artist}>{artist}</h2>
+        </div>
+        {added &&
+          <div className={styles.info}>
+            <div className={styles.added}>
+              <div className={styles.addedName}>Added by</div>
+              <div className={styles.name}>{added && added.name}</div>
+            </div>
+            {added.photo && <img className={styles.photo} src={added && added.photo} alt={added.name} />}
+          </div>}
       </div>
       <Progress isPlaying={isPlaying} progress={progress} duration={duration} />
       <Visualiser isPlaying={isPlaying} />
     </div>
-    {added &&
-      <div className={styles.info}>
-        <img
-          className={styles.photo}
-          src={added && added.photo}
-          alt={added.name}
-        />
-          <div className={styles.addedName}>Added by</div>
-        <div className={styles.added}>
-          <div className={styles.name}>{added && added.name}</div>
-        </div>
-      </div>}
   </div>
 );
