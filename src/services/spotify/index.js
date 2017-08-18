9534c23acd6008131a79dcc090a5b89e3b910521
Taylor Winfield <taylorwinfield4@gmail.com>
2017-08-18 18:17:06 +0100

    Fix Spotify progress

--- src/services/spotify/index.js
+++ src/services/spotify/index.js
@@ -15,9 +15,9 @@ export async function getCurrentTrack() {
         }
       }
     )).json();
-    const { item, context: { uri } } = response;
+    const { item, context: { uri }, progress_ms, is_playing } = response;
     const user = await getUserFromPlaylist(parsePlaylistURI(uri), item.id);
-    return transform({ ...user, ...item });
+    return transform({ ...item, ...user, progress_ms, is_playing });
   } catch (error) {
     console.log(error);
     throw new Error(error);
@@ -89,71 +89,3 @@ async function getUserFromId(id) {
     throw new Error(error);
   }
 }
-
-// 1. Get current track
-// 2. Get playlist id
-// 3. Get user who added current track on playlist
-// 4. if exists, return, if not null
-// 5. return track with user added augmented
-
-// export async function getPlaylistTracks(userID, playlistID, offset = 0) {
-//   try {
-//     const accessToken = localStorage.getItem(SPOTIFY_ACCESS_TOKEN);
-//     const response = await fetch(
-//       `${apiURL}/users/${userID}/playlists/${playlistID}/tracks`,
-//       {
-//         method: "GET",
-//         headers: {
-//           Authorization: `Bearer ${accessToken}`
-//         }
-//       }
-//     );
-//     console.log(response.json());
-//   } catch (e) {
-//     let moreTracks = true;
-//
-//     while (moreTracks) {
-//       moreTracks = false;
-//     }
-//   }
-// }
-
-// export async function getAllPlaylistTracks(userID, playlistID) {
-//   let tracks = [];
-//   let totalTracks = 1;
-//
-//   while (tracks.length < totalTracks) {
-//     const newTracks = await this.getPlaylistTracks(
-//       userID,
-//       playlistID,
-//       tracks.length
-//     );
-//     const { items = [] } = newTracks;
-//     tracks = [...tracks, ...items];
-//     totalTracks = newTracks.total;
-//   }
-//
-//   return tracks;
-// }
-
-// export async function getAllPlaylistTracks(userID, playlistID) {
-//   let tracks = [];
-// }
-
-// export async function getUserFromPlaylist(userID, playlistID, trackID) {
-//   const tracks = await this.getAllPlaylistTracks(userID, playlistID);
-//   return tracks.find(trackData => {
-//     return trackData.track.id === trackID;
-//   });
-// }
-
-// export async function getUser(userID) {
-//   const accessToken = localStorage.getItem(SPOTIFY_ACCESS_TOKEN);
-//   const response = await fetch(`${apiURL}/users/${userID}`, {
-//     method: "GET",
-//     headers: {
-//       Authorization: `Bearer ${accessToken}`
-//     }
-//   });
-//   return response.json();
-// }
--- src/services/spotify/transform.js
+++ src/services/spotify/transform.js
@@ -1,23 +1,23 @@
-import nn from "nevernull";
-
 export default (
   {
     is_playing: isPlaying,
     progress_ms: progress,
     duration_ms: duration,
     name: title,
-    album: { artists: [{ name }], images: artwork },
-    user
+    album: { artists: [{ name: artist }], images: artwork },
+    display_name,
+    id,
+    images
   }
 ) => ({
   isPlaying,
   progress,
   duration,
   title,
-  name,
+  artist,
   artwork,
   added: {
-    name: nn(user).display_name() || nn(user).id(),
-    photo: nn(user).images[0].url()
+    name: display_name || id,
+    photo: !!images.length && images[0].url
   }
 });
