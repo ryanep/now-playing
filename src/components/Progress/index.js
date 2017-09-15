0f0e3bb01738aaf06d62744f68d0df30cce66a7e
Taylor Winfield <taylorwinfield4@gmail.com>
2017-09-15 20:08:41 +0100

    Simplify progress component

--- src/components/Progress/index.js
+++ src/components/Progress/index.js
@@ -1,33 +1,6 @@
 import React, { Component } from "react";
-import styles from './style.scss';
+import styles from "./style.scss";
 
-export default class Progress extends Component {
-
-	constructor(props) {
-		super(props);
-		this.state = {
-			progress: props.progress / 1000
-		};
-		this.updateProgress();
-	}
-
-	componentWillReceiveProps(nextProps) {
-		this.setState({ progress: nextProps.progress / 1000 });
-	}
-
-	updateProgress() {
-		setTimeout(() => {
-			if (this.props.isPlaying) {
-				this.setState({ progress: this.state.progress + 0.5 });
-			}
-			this.updateProgress();
-		}, 500);
-	}
-
-	render() {
-		return (
-			<progress value={this.state.progress} max={this.props.duration / 1000} className={styles.progress}></progress>
-		)
-	}
-
-}
+export default ({ progress, duration }) => (
+  <div value={progress} max={duration / 1000} className={styles.progress} />
+);
