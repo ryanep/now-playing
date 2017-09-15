d937cbb5b7356dd9b2a5f71248ba569cc7f57093
Taylor Winfield <taylorwinfield4@gmail.com>
2017-09-15 21:07:16 +0100

    add access token expired test

--- src/actions/auth.spec.js
+++ src/actions/auth.spec.js
@@ -1,14 +1,14 @@
-import * as authActions from "./auth";
-import * as actionTypes from "../constants/action-types";
+import * as authActions from './auth';
+import * as actionTypes from '../constants/action-types';
 
-describe("actions/auth", () => {
-  const access_token = "__access_token__";
-  const refresh_token = "__refresh_token__";
-  const error = "__error__";
-  const code = "__code__";
+describe('actions/auth', () => {
+  const access_token = '__access_token__';
+  const refresh_token = '__refresh_token__';
+  const error = '__error__';
+  const code = '__code__';
 
-  describe("accessTokenRequested", () => {
-    it("returns ACCESS_TOKEN_REQUEST as the action type", () => {
+  describe('accessTokenRequested', () => {
+    it('returns ACCESS_TOKEN_REQUEST as the action type', () => {
       expect(authActions.accessTokenRequested(code)).toMatchObject({
         type: actionTypes.ACCESS_TOKEN_REQUEST
       });
@@ -16,15 +16,15 @@ describe("actions/auth", () => {
 
     it('returns the code in the payload', () => {
       expect(authActions.accessTokenRequested(code)).toMatchObject({
-        payload:  {
+        payload: {
           code
         }
       });
     });
   });
 
-  describe("accessTokenSuccess", () => {
-    it("returns ACCESS_TOKEN_SUCCESS as the action type", () => {
+  describe('accessTokenSuccess', () => {
+    it('returns ACCESS_TOKEN_SUCCESS as the action type', () => {
       expect(
         authActions.accessTokenSuccess({ access_token, refresh_token })
       ).toMatchObject({
@@ -32,7 +32,7 @@ describe("actions/auth", () => {
       });
     });
 
-    it("returns the provided access token and refresh token in the payload", () => {
+    it('returns the provided access token and refresh token in the payload', () => {
       expect(
         authActions.accessTokenSuccess({ access_token, refresh_token })
       ).toMatchObject({
@@ -44,42 +44,50 @@ describe("actions/auth", () => {
     });
   });
 
-  describe("accessTokenFailure", () => {
-    it("returns ACCESS_TOKEN_FAILURE as the action type", () => {
+  describe('accessTokenFailure', () => {
+    it('returns ACCESS_TOKEN_FAILURE as the action type', () => {
       expect(authActions.accessTokenFailure(error)).toMatchObject({
         type: actionTypes.ACCESS_TOKEN_FAILURE
       });
     });
 
-    it("returns the error in the payload", () => {
+    it('returns the error in the payload', () => {
       expect(authActions.accessTokenFailure(error)).toMatchObject({
         payload: new Error(error)
       });
     });
 
-    it("sets the error key to true", () => {
+    it('sets the error key to true', () => {
       expect(authActions.accessTokenFailure(error)).toMatchObject({
         error: true
       });
     });
   });
 
-  describe("refreshTokenRequested", () => {
-    it("returns REFRESH_TOKEN_REQUEST as the action type", () => {
+  describe('accessTokenExpired', () => {
+    it('returns ACCESS_TOKEN_FAILURE as the action type', () => {
+      expect(authActions.accessTokenExpired()).toEqual({
+        type: actionTypes.ACCESS_TOKEN_EXPIRED
+      });
+    });
+  });
+
+  describe('refreshTokenRequested', () => {
+    it('returns REFRESH_TOKEN_REQUEST as the action type', () => {
       expect(authActions.refreshTokenRequested()).toEqual({
         type: actionTypes.REFRESH_TOKEN_REQUEST
       });
     });
   });
 
-  describe("refreshTokenSuccess", () => {
-    it("returns REFRESH_TOKEN_SUCCESS as the action type", () => {
+  describe('refreshTokenSuccess', () => {
+    it('returns REFRESH_TOKEN_SUCCESS as the action type', () => {
       expect(authActions.refreshTokenSuccess({ access_token })).toMatchObject({
         type: actionTypes.REFRESH_TOKEN_SUCCESS
       });
     });
 
-    it("returns the access token in the payload", () => {
+    it('returns the access token in the payload', () => {
       expect(authActions.refreshTokenSuccess({ access_token })).toMatchObject({
         payload: {
           accessToken: access_token
@@ -88,20 +96,20 @@ describe("actions/auth", () => {
     });
   });
 
-  describe("refreshTokenFailure", () => {
-    it("returns REFRESH_TOKEN_FAILURE as the action type", () => {
+  describe('refreshTokenFailure', () => {
+    it('returns REFRESH_TOKEN_FAILURE as the action type', () => {
       expect(authActions.refreshTokenFailure(error)).toMatchObject({
         type: actionTypes.REFRESH_TOKEN_FAILURE
       });
     });
 
-    it("returns the error in the payload", () => {
+    it('returns the error in the payload', () => {
       expect(authActions.refreshTokenFailure(error)).toMatchObject({
         payload: new Error(error)
       });
     });
 
-    it("sets the error key to true", () => {
+    it('sets the error key to true', () => {
       expect(authActions.refreshTokenFailure(error)).toMatchObject({
         error: true
       });
